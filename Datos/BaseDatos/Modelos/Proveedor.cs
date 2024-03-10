61a2ef57ecdb710330feeb900efda23bb28937cb
LeonelHerculesIntermoda <82479435+LeonelHerculesIntermoda@users.noreply.github.com>
2024-03-10 12:35:25 -0600

    Adicion logica de proveedores

---
+++ Datos/BaseDatos/Modelos/Proveedor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.BaseDatos.Modelos
+{
+    public class Proveedor
+    {
+        [Key]
+        public int ProveedorId { get; set; }
+    }
+}
